8003796cd34e441a0d7d32191236c123ed687546
PoeticCrane <poeticcrane@gmail.com>
2023-04-19 16:05:10 +0300

    change ReadMe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router';
 import './App.css';
+import Index from './pages/Index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path={'/'} exact element={<Navigate to={'/index'} replace />} />
+      <Route path={'/index'} element={<Index />} />
+      {/* <Route path={'/my-works'} element={<MyWorks />} />
+
+      <Route path={'/about-me'} element={<AboutMe />} />
+
+      <Route path={'/contact-me'} element={<ContactMe />} /> */}
+
+      {/* <Route path='*' element={<NotFound />} /> */}
+    </Routes>
   );
 }
 
